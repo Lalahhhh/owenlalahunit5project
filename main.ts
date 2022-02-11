b23de521bbc28c6a27e1afbafe974292b6e71b1c
Lalahhhh <91301340+Lalahhhh@users.noreply.github.com>
2022-02-11 12:08:31 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
-scene.setBackgroundColor(9)
+game.showLongText("Pythagorean Theorem Solver: ", DialogLayout.Center)
+scene.setBackgroundColor(13)
+let Side_1 = game.askForNumber("")
+let Side_2 = game.askForNumber("")
