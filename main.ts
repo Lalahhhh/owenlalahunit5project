53bc811ceb2cdc7fd20c4bdf1c7d4246b06ddf78
Lalahhhh <91301340+Lalahhhh@users.noreply.github.com>
2022-02-11 12:32:45 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+// dont forget to add comment!!!
 scene.setBackgroundColor(13)
 game.showLongText("Pythagorean Theorem Solver when you are given A & B: Enter two the measurements of two sides of your triangle ", DialogLayout.Center)
 pause(1000)
@@ -5,4 +6,11 @@ game.showLongText("Enter side 1 (click A to continue)", DialogLayout.Center)
 let Side_1 = game.askForNumber("")
 game.showLongText("Enter side 2", DialogLayout.Center)
 let Side_2 = game.askForNumber("")
-let side_3 = game.askForNumber("")
+let side_1_answer = Side_1 * Side_1
+let side_2_answer = Side_2 * Side_2
+let Side_3_answer = Math.sqrt(side_1_answer + side_2_answer)
+game.showLongText("Calculating...", DialogLayout.Center)
+pause(1000)
+game.showLongText("The final answer of side 3 is...", DialogLayout.Center)
+pause(1000)
+game.showLongText(Side_3_answer, DialogLayout.Center)
